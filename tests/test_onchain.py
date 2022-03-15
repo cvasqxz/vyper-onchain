62f515e746c19ce2504bb668b686155fe47b04d5
cvasqxz <cvxz@pm.me>
2022-03-15 12:49:16 -0300

    Full collection mint test

--- tests/test_onchain.py
+++ tests/test_onchain.py
@@ -6,6 +6,9 @@ def onchain_contract(accounts, Onchain):
 
 
 def test_mint(accounts, onchain_contract):
-    assert onchain_contract.mint()
-    assert onchain_contract.tokenURI(0) == 'miau'
-    assert onchain_contract.ownerOf(0) == accounts[0]
+    max_supply = onchain_contract.maxSupply()
+    for i in range(max_supply):
+        assert onchain_contract.mint()
+        assert onchain_contract.tokenURI(0) == 'miau'
+        assert onchain_contract.ownerOf(0) == accounts[0]
+
